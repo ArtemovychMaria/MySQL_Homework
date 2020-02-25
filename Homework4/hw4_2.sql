f5a1b5c750cd55c8937d50942736827f6d132e3f
Artemovych Maria <mari4ka.art.bloody@gmail.com>
2020-02-25 22:55:46 +0200

    added elements into tables

--- Homework4/hw4_2.sql
+++ Homework4/hw4_2.sql
@@ -17,7 +17,7 @@ customer_id int not null,
 category_id int not null,
 quality_id int not null,
 factory_id int not null,
-garanty_id int not null
+garanty_id int
 );
 
 create table categories(
@@ -57,5 +57,64 @@ alter table customers add foreign key(country_id) references countries(id);
 alter table orders add foreign key(quality_id) references qualities(id);
 alter table orders add foreign key(factory_id) references factories(id);
 alter table orders add foreign key(garanty_id) references garanties(id);
+
+insert into categories(name)
+values
+("Електротехніка"),
+("Іграшки"),
+("Побутова хімія");
+
+insert into towns(name)
+values
+("Львів"),
+("Київ"),
+("Берлін");
+
+insert into countries(name)
+values
+("Німеччина"),
+("Україна"),
+("Польща");
+
+insert into qualities(type)
+values
+("Висока"),
+("Середня"),
+("Низька");
+
+insert into factories(name)
+values
+("Електрон"),
+("Дитячий світ"),
+("Єва");
+
+insert into garanties(years)
+values
+(5),
+(3),
+(2);
+
+insert into customers(customer_name,customer_surname,town_id,country_id)
+values
+("Іван","Петренко",1,2),
+("Сергій","Іващук",2,2),
+("Анна","Хоменко",3,1);
+
+insert into orders(name,customer_id,category_id,quality_id,factory_id,garanty_id)
+values
+("Ведмедик",1,2,1,2,3),
+("Телевізор",1,1,2,1,1),
+("Пральна машина",2,1,2,1,2),
+("Праска",3,1,1,1,1);
+
+select * from orders;
+select * from customers;
+select * from categories;
+select * from factories;
+select * from countries;
+select * from towns;
+select * from garanties;
+select * from qualities;
+
 
 
